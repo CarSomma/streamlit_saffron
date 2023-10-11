a7c3fe32d2033b8229a582ca483a71a545dc1239
Carmine <carsomma@gmail.com>
2023-10-11 11:45:39 +0200

    added solution_excercise branch

--- pages/predict.py
+++ pages/predict.py
@@ -19,34 +19,37 @@ import pandas as pd
 
 # Task1:
 # load the model
-with open("...", "rb") as file:
-    model = pickle...
+with open("/pages/model_penguins.pkl", "rb") as file:
+    model = pickle.load(file)
+print(model)
 
 # Task2:
 # Set the value of flipper_length to a number between 160 and 240
-flipper_lenght = ...
+flipper_lenght = 170
 
 # Set the value of species to one of the possible options 'Adelie','Chinstrap','Gentoo'
-species = ...
+species = 'Gentoo'
 
 # Set the value of sex to 'Female' or 'Male'
-sex = ...
+sex = 'Female'
 
 # Task3:
 # Let's create a dictionary having as keys the input features 
 # (see initial doc string at the top) 
 # and corresponding values the one we have set just above
 input_dict_X = {
-    ...
+    'flipper_length_mm':flipper_lenght,
+    'species':species,
+    'sex':sex 
 }
 print(input_dict_X)
 
 # Task4:
 # create a dataframe from the input_dict_X 
-input_X = ...
+input_X = pd.DataFrame(data=input_dict_X, index=[0])
 
 # Task5:
 # make prediction with the model
-y_pred = ...
+y_pred = model.predict(input_X)
 
 print(y_pred)
